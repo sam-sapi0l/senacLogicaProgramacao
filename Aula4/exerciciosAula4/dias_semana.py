231cf59300666248afc5ac0bb36b896e2b8f3acb
bob shell <bob-shell@kali.th3g3ntl3m3n>
2024-04-04 08:35:46 -0300

    Exercicios de condicional - Exercicio 9 completo

--- Aula4/exerciciosAula4/dias_semana.py
+++ Aula4/exerciciosAula4/dias_semana.py
@@ -4,3 +4,24 @@
 
 '''
 
+
+dia_digitado = int(input('Digite um número de [1-7]: '))
+dia = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado']
+
+
+if dia_digitado == 1:
+    print(f'{dia[0]}')
+elif dia_digitado == 2:
+    print(f'{dia[1]}')
+elif dia_digitado == 3:
+    print(f'{dia[2]}')
+elif dia_digitado == 4:
+    print(f'{dia[3]}')
+elif dia_digitado == 5:
+    print(f'{dia[4]}')
+elif dia_digitado == 6:
+    print(f'{dia[5]}')
+elif dia_digitado == 7:
+    print(f'{dia[6]}')
+else:
+    print('Valor inválido!')       
