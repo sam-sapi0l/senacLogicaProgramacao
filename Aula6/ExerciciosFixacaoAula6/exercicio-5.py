fcc78279e7adfdcef4c3dfac5c58492d4166d58e
Gabriel Dallemole <143417447+sam-sapi0l@users.noreply.github.com>
2024-04-24 18:58:04 -0300

    Exercicios de Fixação

---
+++ Aula6/ExerciciosFixacaoAula6/exercicio-5.py
@@ -0,0 +1,15 @@
+﻿'''
+Contagem Regressiva com Loop While: Crie um programa que inicie em 10 e conte regressivamente até 1 usando um loop while. Após a contagem, exiba uma mensagem, por exemplo, "Foguete lançado!".
+'''
+
+contador = 10
+while contador > -1:
+    print(contador)
+    contador -= 1
+print('Finalizada contagem!')
+
+#############################
+# Loop For
+for i in range(10, -1, -1):
+    print(i)
+print('Finalizada contagem regressiva!')
