6c8b5501f9466dc10e972d4a0d8b55977d65d4cf
Gabriel Dallemole <143417447+sam-sapi0l@users.noreply.github.com>
2024-04-24 18:50:07 -0300

    Delete ExerciciosFixacaoAula6 directory

--- ExerciciosFixacaoAula6/exercicio-3.py
+++
@@ -1,9 +0,0 @@
-﻿'''
-Programa de Tabuada com Loop For: Faça um programa que peça ao usuário um número e, em seguida, use um loop for para exibir a tabuada desse número, de 1 a 10.
-
-'''
-
-numero = int(input('Digite um número: '))
-for i in range(1, 11):
-    tabuada = i * numero
-    print(tabuada)
